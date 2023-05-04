edcae75cba75cbb892e0287143f03aa493d36de6
Josue Baez <josue.baez@coderhouse.com>
2023-05-03 22:54:16 -0300

    ADD, login component tests

---
+++ src/app/auth/mocks/auth-service.mock.ts
@@ -0,0 +1,25 @@
+import { BehaviorSubject, Observable, of } from "rxjs";
+import { LoginFormValue } from "../services/auth.service";
+import { Usuario } from "src/app/core/models";
+
+export const USUARIO_ADMIN_MOCK: Usuario = {
+  id: 1,
+  apellido: 'testapellido',
+  email: 'test@mail.com',
+  nombre: 'testnombre',
+  role: 'admin',
+  token: 'asdkjsanfkdams3u2hjdasfadsuh'
+}
+
+export class AuthServiceMock {
+
+  private authUser$ = new BehaviorSubject<Usuario | null>(null);
+
+  login(formValue: LoginFormValue): void {
+    this.authUser$.next(USUARIO_ADMIN_MOCK);
+  }
+
+  verificarToken(): Observable<boolean> {
+    return of(true);
+  }
+}
